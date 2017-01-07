3f959d589b77a50e6bc1560be37c8a0da96d97f7
Mathew James <mathew.james@ipo.gov.uk>
2017-01-07 19:41:50 +0000

    more refactor

--- ShoppingCartInterviewTestInC/Checkout.cs
+++ ShoppingCartInterviewTestInC/Checkout.cs
@@ -11,7 +11,7 @@
 
         public Cart AddToCart(Product product)
         {
-            var cartItem = Cart.CartItems.Find(x => x.Product.ProductKey == product.ProductKey);
+            var cartItem = ProductExistInCart(product);
 
             if (cartItem == null)
             {
@@ -23,6 +23,11 @@
             }
 
             return Cart;
+        }
+
+        private CartItem ProductExistInCart(Product product)
+        {
+            return Cart.CartItems.Find(x => x.Product.ProductKey == product.ProductKey);
         }
 
         public Cart RemoveFromCart(CartItem cartItem)
